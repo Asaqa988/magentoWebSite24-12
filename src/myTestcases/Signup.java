a9151482e7af11a3f98e0c49b2ea6bd18babb604
Asaqa988 <asaqa001@gmail.com>
2024-01-29 19:22:21 +0300

    the first four test cases

--- src/myTestcases/Signup.java
+++ src/myTestcases/Signup.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Signup  extends Parameters{
+public class Signup extends Parameters {
 
 	WebDriver driver = new ChromeDriver();
 
@@ -21,37 +21,91 @@ public class Signup  extends Parameters{
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 	}
 
-	@Test()
+	@Test(priority = 1)
 	public void myFirstTest() throws InterruptedException {
 
 		driver.get("https://magento.softwaretestingboard.com/");
-		
-		driver.findElement(By.linkText("Create an Account")).click(); 
-		
-		
-		// find the elements 
-		WebElement FirstName  = driver.findElement(By.id("firstname")); 
-		WebElement LastName = driver.findElement(By.id("lastname")); 
-		WebElement Email  = driver.findElement(By.id("email_address")); 
-		WebElement Password   = driver.findElement(By.id("password")) ;
-		WebElement confirmPassword = driver.findElement(By.id("password-confirmation")) ; 
-		
-		WebElement createAccountButtonElement = driver.findElement(By.cssSelector("button[title='Create an Account'] span")) ;
-		
+
+		driver.findElement(By.linkText("Create an Account")).click();
+
+		// find the elements
+		WebElement FirstName = driver.findElement(By.id("firstname"));
+		WebElement LastName = driver.findElement(By.id("lastname"));
+		WebElement Email = driver.findElement(By.id("email_address"));
+		WebElement Password = driver.findElement(By.id("password"));
+		WebElement confirmPassword = driver.findElement(By.id("password-confirmation"));
+
+		WebElement createAccountButtonElement = driver
+				.findElement(By.cssSelector("button[title='Create an Account'] span"));
+
 		// interact with the elements
-		
-		FirstName.sendKeys(FirstNames[randomIndex]); 
+
+		FirstName.sendKeys(FirstNames[randomIndex]);
 		LastName.sendKeys(lastNames[randomIndex]);
 		Email.sendKeys(emailID);
 		Password.sendKeys(CommonPassword);
-		confirmPassword.sendKeys(CommonPassword); 
-		
-		createAccountButtonElement.click(); 
-		
+		confirmPassword.sendKeys(CommonPassword);
+
+		createAccountButtonElement.click();
+
 		String WelcomeMsg = driver.findElement(By.className("message-success")).getText();
-		
+
 		assertEquals(WelcomeMsg, "Thank you for registering with Main Website Store.");
-		
+
+	}
+
+	@Test(priority = 2)
+	public void logoutProcess() throws InterruptedException {
+
+		driver.get("https://magento.softwaretestingboard.com/customer/account/logout/");
+		;
+		assertEquals(driver.getCurrentUrl().contains("logoutSuccess"), true);
+	}
+
+	@Test(priority = 3)
+	public void LoginProcess() throws InterruptedException {
+
+		driver.findElement(By.linkText("Sign In")).click();
+		driver.findElement(By.id("email")).sendKeys(emailID);
+		driver.findElement(By.id("pass")).sendKeys(CommonPassword);
+
+		driver.findElement(By.id("send2")).click();
+
+		Thread.sleep(2000);
+		String WelcomeMessage = driver.findElement(By.cssSelector(".greet.welcome")).getText();
+		assertEquals(WelcomeMessage.contains("Welcome"), true);
+
+	}
+
+	@Test(priority = 4)
+	public void addOneRandomItem() throws InterruptedException {
+		driver.get("https://magento.softwaretestingboard.com/");
+
+		WebElement ItemsContainer = driver.findElement(By.cssSelector(".product-items.widget-product-grid"));
+
+		int numberofitems = ItemsContainer.findElements(By.tagName("li")).size();
+		System.out.println(numberofitems);
+
+		int randomItemToSelect = rand.nextInt(4);
+		ItemsContainer.findElements(By.tagName("li")).get(randomItemToSelect).click();
+
+		WebElement sizesContainer = driver.findElement(By.cssSelector(".swatch-attribute.size"));
+
+		int theSizes = sizesContainer.findElements(By.className("swatch-option")).size();
+		sizesContainer.findElements(By.className("swatch-option")).get(rand.nextInt(theSizes)).click();
+		Thread.sleep(2000);
+
+		WebElement colorsContainer = driver.findElement(By.cssSelector(".swatch-attribute.color"));
+		int theColors = colorsContainer.findElements(By.className("swatch-option")).size();
+		colorsContainer.findElements(By.className("swatch-option")).get(rand.nextInt(theColors)).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("product-addtocart-button")).click();
+		Thread.sleep(3000);
+
+		String ActualMsg = driver.findElement(By.cssSelector(".page.messages")).getText();
+
+		assertEquals(ActualMsg.contains("You added"), true);
+
 	}
 
 }
